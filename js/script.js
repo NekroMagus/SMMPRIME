041b3fbc9912dd38cbad9fa890a06c0d220da073
NekroMagus <NekroMagus@mail.ru>
2019-12-14 19:02:50 +0300

    add script

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    var letCollapseWidth = false,
+        paddingValue = 30,
+        sumWidth = $('.navbar-right-block').width() + $('.navbar-left-block').width() + $('.navbar-brand').width() + paddingValue;
+
+    $(window).on('resize', function () {
+        navbarResizerFunc();
+    });
+
+    var navbarResizerFunc = function navbarResizerFunc() {
+        if (sumWidth <= $(window).width()-5000) {
+            if (letCollapseWidth && letCollapseWidth <= $(window).width()) {
+                $('#navbar').addClass('navbar-collapse');
+                $('#navbar').removeClass('navbar-collapsed');
+                $('nav').removeClass('navbar-collapsed-before');
+                letCollapseWidth = false;
+            }
+        } else {
+            $('#navbar').removeClass('navbar-collapse');
+            $('#navbar').addClass('navbar-collapsed');
+            $('nav').addClass('navbar-collapsed-before');
+            letCollapseWidth = $(window).width();
+        }
+    };
+
+    if ($(window).width() >= 768) {
+        navbarResizerFunc();
+    }
+});
